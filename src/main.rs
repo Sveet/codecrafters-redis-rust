f05fb1b49a41a80d2d55f70366d827b5d9748cba
Joseph Fortman <jdbf89@gmail.com>
2023-07-20 16:59:54 -0500

    respond to all requests with PONG

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                let response = "+PONG\r\n".as_bytes();
+                let _ = _stream.write(response);
             }
             Err(e) => {
                 println!("error: {}", e);
