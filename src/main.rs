ff1e2cb17288fb215a98d08ce395a8dfd43bdf60
Joseph Fortman <jdbf89@gmail.com>
2023-08-06 21:27:08 -0500

    use a non-blocking tcp listener to create a single threaded event loop

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,52 @@
 use std::{
-    io::{Read, Write},
+    io::{ErrorKind, Read, Write},
     net::{TcpListener, TcpStream},
 };
 
 fn main() {
+    let mut clients: Vec<TcpStream> = vec![];
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    listener
+        .set_nonblocking(true)
+        .expect("Cannot set non-blocking on listener");
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => {
+    loop {
+        match listener.accept() {
+            Ok(_stream) => {
                 println!("accepted new connection");
-
-                let mut buf = [0; 512];
-                loop {
-                    let read_count = _stream
-                        .read(&mut buf)
-                        .expect("Error reading stream into buffer");
-                    if read_count == 0 {
-                        break;
-                    }
-                    handle_stream(&mut _stream);
-                }
+                _stream
+                    .0
+                    .set_nonblocking(true)
+                    .expect("Cannot set non-blocking on socket");
+                clients.push(_stream.0);
             }
             Err(e) => {
-                println!("error: {}", e);
+                if e.kind() != ErrorKind::WouldBlock {
+                    println!("error: {}", e);
+                }
             }
+        }
+
+        let mut disconnected: Vec<usize> = vec![];
+        for (i, client) in clients.iter_mut().enumerate() {
+            let mut buf = [0; 512];
+            match client.read(&mut buf) {
+                Ok(_) => {
+                    handle_stream(client);
+                }
+                Err(e) => {
+                    if e.kind() == ErrorKind::WouldBlock {
+                        continue;
+                    } else {
+                        println!("error: {}", e);
+                        disconnected.push(i);
+                    }
+                }
+            }
+        }
+
+        for &index in disconnected.iter().rev() {
+            clients.remove(index);
         }
     }
 }
