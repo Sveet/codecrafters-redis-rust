eaec6fb603440769ca99401eef032f43b0eccf0c
Joseph Fortman <jdbf89@gmail.com>
2023-08-06 19:36:06 -0500

    handle multiple messages in a single connection

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
                 println!("accepted new connection");
-                let response = "+PONG\r\n".as_bytes();
-                let _ = _stream.write(response);
+
+                let mut buf = [0; 512];
+                loop {
+                    let read_count = _stream
+                        .read(&mut buf)
+                        .expect("Error reading stream into buffer");
+                    if read_count == 0 {
+                        break;
+                    }
+                    handle_stream(&mut _stream);
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_stream(stream: &mut TcpStream) {
+    let response = "+PONG\r\n".as_bytes();
+    let _ = stream.write(response);
 }
